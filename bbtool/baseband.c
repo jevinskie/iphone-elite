78e523f8fc60a5f516265c786cfbac28c2b87651
onitake <onitake@609ea2eb-153c-0410-89cf-374ace84af61>
2007-12-07 17:50:08 +0000

    Lots of fixes and updates, reading, erasing and writing works in many cases, but there's still a problem with writing the first two flash blocks. Further analysis of bbupdater is neccessary. I think it's possible to write blocks of data while it's still erasing? Is that what bbupdater does?

--- bbtool/baseband.c
+++ bbtool/baseband.c
@@ -31,7 +31,6 @@
 #include "baseband.h"
 
 const char *DEFAULT_BASEBAND_DEVICE = "/dev/tty.baseband";
-const unsigned int SECPACK_SIZE = 2048;
 
 static const int DEFAULT_SPEED = 115200;
 static const int DEFAULT_TIMEOUT = 1;
@@ -79,6 +78,12 @@ typedef struct {
 	unsigned int unknown2;
 } SecpackReplyPacket;
 static const int SECPACK_REPLY_PACKET_SIZE = 6;
+
+typedef struct {
+	unsigned short status;
+	unsigned short error;
+} WriteReplyPacket;
+static const int WRITE_REPLY_PACKET_SIZE = 4;
 
 #pragma pack()
 
@@ -164,25 +169,52 @@ static unsigned short checksum(CmdPacket *packet, void *data) {
 	return checksum;
 }
 
+// returns a pointer to the data, or NULL if the data was invalid
+static void *verifyPacket(void *buffer, size_t length) {
+	if (length < CMD_PACKET_SIZE + CMD_PACKET_END_SIZE) {
+		LOG(LOGLEVEL_WARN, "Invalid packet size: %lu (minimum is %u)\n", length, CMD_PACKET_SIZE + CMD_PACKET_END_SIZE);
+		return NULL;
+	}
+	CmdPacket *packet = (CmdPacket *) buffer;
+	if (packet->w02 != 2) {
+		LOG(LOGLEVEL_WARN, "Invalid header: 0x%04x\n", packet->w02);
+		return NULL;
+	}
+	if (length < packet->dataSize + CMD_PACKET_SIZE + CMD_PACKET_END_SIZE) {
+		LOG(LOGLEVEL_WARN, "Invalid packet size: %lu (expected is %u)\n", length, packet->dataSize + CMD_PACKET_SIZE + CMD_PACKET_END_SIZE);
+		return NULL;
+	}
+	char *data = &((char *) buffer)[CMD_PACKET_SIZE];
+	CmdPacketEnd *end = (CmdPacketEnd *) (&data[packet->dataSize]);
+	if (checksum(packet, data) != end->checksum) {
+		LOG(LOGLEVEL_WARN, "Invalid checksum: 0x%04x\n", end->checksum);
+		return NULL;
+	}
+	if (end->w03 != 3) {
+		LOG(LOGLEVEL_WARN, "Invalid footer: 0x%04x\n", end->w03);
+		return NULL;
+	}
+	LOG(LOGLEVEL_DEBUG, "Packet command: 0x%04x\n", packet->cmd);
+	return data;
+}
+
 static void writePacket(int fd, short cmd, void *data, size_t length) {
-	CmdPacket packet = {
-		.w02 = 2,
-		.cmd = cmd,
-		.dataSize = length
-	};
-	CmdPacketEnd end = {
-		.checksum = checksum(&packet, data),
-		.w03 = 3
-	};
-
+	char *buffer = malloc(PACKET_SIZE(length));
+	CmdPacket *packet = (CmdPacket *) buffer;
+	packet->w02 = 2;
+	packet->cmd = cmd;
+	packet->dataSize = length;
+	memcpy(buffer + CMD_PACKET_SIZE, data, length);
+	CmdPacketEnd *end = (CmdPacketEnd *) (buffer + CMD_PACKET_SIZE + length);
+	end->checksum = checksum(packet, data);
+	end->w03 = 3;
+	
 	LOG(LOGLEVEL_TRACE, "Writing packet:\n");
-	LOGDO(LOGLEVEL_TRACE, printBuffer(&packet, CMD_PACKET_SIZE));
-	LOGDO(LOGLEVEL_TRACE, printBuffer(data, packet.dataSize));
-	LOGDO(LOGLEVEL_TRACE, printBuffer(&end, CMD_PACKET_END_SIZE));
+	LOGDO(LOGLEVEL_TRACE, printBuffer(&buffer, PACKET_SIZE(length)));
 
-	write(fd, &packet, CMD_PACKET_SIZE);
-	write(fd, data, packet.dataSize);
-	write(fd, &end, CMD_PACKET_END_SIZE);
+	write(fd, buffer, PACKET_SIZE(length));
+	
+	free(buffer);
 }
 
 static size_t readPacket(int fd, int timeout, void *buffer, size_t length) {
@@ -195,8 +227,10 @@ static size_t readPacket(int fd, int timeout, void *buffer, size_t length) {
 	};
 	
 	size_t hlen = 0;
+	//LOG(LOGLEVEL_INFO, "length=%lu hlen=%lu\n", length, hlen);
 	while (length - hlen > 0 && select(fd + 1, &nfd, 0, 0, &tv) > 0) {
-		hlen += read(fd, buffer + hlen, length);
+		hlen += read(fd, buffer + hlen, length - hlen);
+		//LOG(LOGLEVEL_INFO, "hlen=%lu\n", hlen);
 		//LOGDO(LOGLEVEL_DEBUG, printBuffer(buffer, hlen));
 	}
 	
@@ -246,27 +280,6 @@ int openBaseband(const char *device) {
 	return fd;
 }
 
-// returns a pointer to the data, or NULL if the data was invalid
-static void *verifyPacket(void *buffer, size_t length) {
-	if (length < CMD_PACKET_SIZE + CMD_PACKET_END_SIZE) {
-		LOG(LOGLEVEL_WARN, "Invalid packet size: %lu\n", length);
-		return NULL;
-	}
-	CmdPacket *packet = (CmdPacket *) buffer;
-	if (length < packet->dataSize + CMD_PACKET_SIZE + CMD_PACKET_END_SIZE) {
-		LOG(LOGLEVEL_WARN, "Invalid packet size: %lu\n", length);
-		return NULL;
-	}
-	char *data = &((char *) buffer)[CMD_PACKET_SIZE];
-	CmdPacketEnd *end = (CmdPacketEnd *) (&data[packet->dataSize]);
-	if (checksum(packet, data) != end->checksum) {
-		LOG(LOGLEVEL_WARN, "Invalid checksum: 0x%04x\n", end->checksum);
-		return NULL;
-	}
-	LOG(LOGLEVEL_DEBUG, "Packet command: 0x%04x\n", packet->cmd);
-	return data;
-}
-
 void setBaudRate(int fd, unsigned int speed) {
 	LOG(LOGLEVEL_INFO, "Increasing baud rate to %dbps...\n", speed);
 	
@@ -290,10 +303,16 @@ VersionPacket getBootVersion(int fd) {
 	char buffer[PACKET_SIZE(VERSION_PACKET_SIZE)];
 	size_t length = readPacket(fd, 5, buffer, sizeof(buffer));
 	VersionPacket *packet = (VersionPacket *) verifyPacket(buffer, length);
-	LOG(LOGLEVEL_DEBUG, "Got bootloader version: %d.%d\n", packet ? packet->major : 0, packet ? packet->minor : 0);
-	//LOGDO(LOGLEVEL_DEBUG, printBuffer(buffer, length));
 	
-	return *packet;
+	if (packet) {
+		LOG(LOGLEVEL_DEBUG, "Got bootloader version: %d.%d\n", packet->major, packet->minor);
+		//LOGDO(LOGLEVEL_DEBUG, printBuffer(buffer, length));
+		return *packet;
+	}
+	
+	VersionPacket zeroBoot;
+	memset(&zeroBoot, 0, VERSION_PACKET_SIZE);
+	return zeroBoot;
 }
 
 void prepareFlash(int fd) {
@@ -327,6 +346,7 @@ size_t readBaseband(int fd, void *buffer, unsigned short size) {
 	LOG(LOGLEVEL_STATUS, "Reading %u bytes from flash...\n", size);
 	
 	writePacket(fd, 0x803, &size, sizeof(size));
+	usleep(500000);
 	void *temp = malloc(PACKET_SIZE(size));
 	size_t length = readPacket(fd, DEFAULT_TIMEOUT, temp, PACKET_SIZE(size));
 	
@@ -386,13 +406,13 @@ void eraseBaseband(int fd, unsigned int begin, unsigned int end) {
 				if (reply->current >= begin && reply->current <= end) {
 					int percent = (reply->current - begin) * 100 / base;
 					if (percent != previous) {
-						LOG(LOGLEVEL_INFO, "Current position: %u%%\n", percent);
+						LOG(LOGLEVEL_INFO, "Current progress: %u%%\n", percent);
 						previous = percent;
 					}
 				} else if (reply->current == 0xa0000000 && reply->done) {
 					LOG(LOGLEVEL_ERROR, "Looks like the erase command failed due to an invalid secpack.\n");
 				} else {
-					LOG(LOGLEVEL_INFO, "Current position: unknown\n");
+					LOG(LOGLEVEL_INFO, "Current position: 0x%08x\n", reply->current);
 				}
 			}
 		} while (reply && !reply->done);
@@ -405,10 +425,14 @@ void writeBaseband(int fd, void *data, size_t length) {
 	LOG(LOGLEVEL_STATUS, "Writing %lu bytes to flash...\n", length);
 	
 	writePacket(fd, 0x804, data, length);
-	char buffer[PACKET_SIZE(sizeof(unsigned short))];
+	char buffer[PACKET_SIZE(WRITE_REPLY_PACKET_SIZE)];
 	size_t rlength = readPacket(fd, WRITE_TIMEOUT, buffer, sizeof(buffer));
-	unsigned short *ret = verifyPacket(buffer, rlength);
-	LOG(LOGLEVEL_DEBUG, "Write returns: %d\n", ret ? *ret : 0);
+	WriteReplyPacket *ret = verifyPacket(buffer, rlength);
+	if (ret && ret->status == 0) {
+		LOG(LOGLEVEL_DEBUG, "Write returns: %d/%d\n", ret->status, ret->error);
+	} else {
+		LOG(LOGLEVEL_ERROR, "Error flashing %lu bytes: %d\n", length, ret->error);
+	}
 }
 
 void secPack(int fd, void *secpack) {
--- bbtool/baseband.h
+++ bbtool/baseband.h
@@ -1,8 +1,27 @@
-/*
-  Copyright (c) 2007 onitake
-  Thanks & kudos to:
-  - geohot for norz
-  - gray and the dev team for reverse engineering and iUnlock/anySIM
+/*!
+  @header iPhone baseband library
+
+  This library is easily integratable into programs that must access
+  the iPhone baseband flash.
+  
+  As stated in the GPL license below, there is NO WARRANTY, using
+  this code inappropriately can cause permanent damage to your
+  iPhone.
+  
+  Supported commands include:
+  <ul>
+  <li>Reading</li>
+  <li>Writing</li>
+  <li>Sending a secpack</li>
+  </ul>
+  
+  Thanks & kudos to: geohot for reverse engineering iUnlock/anySIM and
+  gray and the dev team for reverse engineering bbupdater and the baseband
+  
+  As the dev team changed their stance towards use of their code, this might be
+  duplicate effort. In fact, I've started working on a modified version
+  of their first opensource release that would build correctly with
+  the 0.5 toolchain, but decided to start over to avoid license issues.
 
   This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
@@ -15,7 +34,10 @@
   GNU General Public License for more details.
 
   You should have received a copy of the GNU General Public License
-  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  along with this program.  If not, see
+  <a href=http://www.gnu.org/licenses/>http://www.gnu.org/licenses/</a>.
+
+  @copyright 2007 onitake
 */
 
 #include <unistd.h>
@@ -31,18 +53,26 @@ enum {
 void setLogLevel(int level);
 int getLogLevel();
 
-// utitility function that will print a hex dump of the given buffer
+/*!
+Utitility function that will print a hex dump of the given buffer
+*/
 void printBuffer(void *buffer, size_t length);
 
-// send a reset signal to the baseband via IOKit
+/*!
+Send a reset signal to the baseband via IOKit
+*/
 void resetBaseband();
 
 const char *DEFAULT_BASEBAND_DEVICE;
-// open the given baseband port and configure the serial port for 115000bps
+/*!
+Open the given baseband port and configure the serial port for 115000bps
+*/
 int openBaseband(const char *device);
 
-// change the baud rate
-// "good" values include 921600 and 750000
+/*!
+Change the baud rate
+"Good" values include 921600 and 750000
+*/
 void setBaudRate(int fd, unsigned int speed);
 
 typedef struct {
@@ -54,43 +84,66 @@ typedef struct {
 	// no \0 terminator!
 	char text[8];
 } VersionPacket;
-// get version information from the baseband
+/*!
+Get version information from the baseband
+*/
 VersionPacket getBootVersion(int fd);
 
-// send the CFI commands (prepare for flashing?)
+/*!
+Send the CFI commands (prepare for flashing?)
+*/
 void prepareFlash(int fd);
 
-// sets the offset for read and write
-// the offset will be updated by those commands
+/*!
+Sets the offset for read and write.
+The offset will be updated by those commands.
+*/
 void seekBaseband(int fd, unsigned int offset);
 
-// like read()
-// maximum size is 2048 (or 1024?) bytes (use multiple calls to get more data)
-// interesting locations:
-// 0xa0020000 start of main firmware
-// 0xa002c000 (original?) firmware version string (SGOLD2 something)
-// 0xa0030000 firmware version string
-// 0xa0310000 last block of main firmware (one block is 0x10000=65536 bytes)
-// 0xa03e0000 seczone (0x800=2048 bytes)
+/*!
+Read a block of data from the current address.
+
+Maximum block size is 2048 (or 1024?) bytes (use multiple calls to get more data).
+
+Interesting locations:
+<ul>
+<li>0xa0020000 start of main firmware</li>
+<li>0xa002c000 (original?) firmware version string (SGOLD2 something)</li>
+<li>0xa0030000 firmware version string</li>
+<li>0xa0310000 last block of main firmware (one block is 0x10000=65536 bytes)</li>
+<li>0xa03a0000 seczone (0x800=2048 bytes) or</li>
+<li>0xa03e0000 seczone (which one is it?)</li>
+</ul>
+*/
 size_t readBaseband(int fd, void *buffer, unsigned short size);
 
-// like write()
-// maximum size is 2048 (or 1024?) bytes (use multiple calls to write more data)
+/*!
+Write a block of data to the current flash address.
+
+Maximum block size is 2048 (or 1024?) bytes (use multiple calls to write more data).
+*/
 void writeBaseband(int fd, void *data, size_t length);
 
-// erase one or multiple flash blocks
-// only certain combinations make sense
-// the boot loader will automatically change the value to a sane range
-// like 0xa0020000 to 0xa0310000 (length 0x2F0000)
+/*!
+Erase one or multiple flash blocks
+
+Only certain combinations make sense.
+The boot loader will automatically change the value to a sane range,
+like 0xa0020000 to 0xa0310000 (length 0x2F0000).
+*/
 void eraseBaseband(int fd, unsigned int begin, unsigned int end);
 
-// send the secpack
-// the block must be exactly 2048 bytes long
-// and match (boot loader 3.9 and before)
-// or supercede (all versions) the baseband firmware currently installed
-const unsigned int SECPACK_SIZE;
+#define SECPACK_SIZE 2048
+/*!
+Send the secpack
+
+The supplied secpack must be exactly 2048 bytes long
+and match (boot loader 3.9 and before)
+or supercede (all versions) the baseband firmware currently installed.
+*/
 void secPack(int fd, void *secpack);
 
-// send the end secpack command (how useful is that, actually?)
-// after flashing
+/*!
+Send the end secpack command (how useful is that, actually?) after flashing.
+*/
 void endSecPack(int fd);
